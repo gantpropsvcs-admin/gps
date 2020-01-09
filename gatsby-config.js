e4ce924d12665c75316ed4b0701de8b0fa70b4a4
gantpropsvcs-admin <58055804+gantpropsvcs-admin@users.noreply.github.com>
2020-01-09 11:33:41 -0700

    first

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yelloecake',
-    siteUrl: 'https://yellowcake.netlify.com'
+    title: 'Gant Professional Services',
+    siteUrl: 'https://gantprofessionalservices.info'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
