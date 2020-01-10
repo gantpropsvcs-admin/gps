09da8c46e1910e0288e1d1df18eb06637afa831e
gantpropsvcs-admin <58055804+gantpropsvcs-admin@users.noreply.github.com>
2020-01-10 12:18:28 -0700

    add about page support templates

--- content/pages/about.md
+++ content/pages/about.md
@@ -1,10 +1,12 @@
 ---
-template: DefaultPage
+template: AboutPage
 slug: about
 title: Who is GPS?
 featuredImage: 'https://ucarecdn.com/b6254577-26bf-4b79-aa9b-f3793e2aebdc/'
 subtitle: This is the blog page subtitle.
+section1:
+section2:
 meta:
   description: This is a meta description.
-  title: Blog Page
+  title: About Page
 ---
---
+++ src/templates/AboutPage.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+import PageHeader from '../components/PageHeader'
+import Content from '../components/Content'
+import Layout from '../components/Layout'
+
+// Export Template for use in CMS preview
+export const AboutPageTemplate = ({
+  title,
+  subtitle,
+  featuredImage,
+  section1,
+  section2,
+  body,
+}) => (
+  <main>
+    <PageHeader
+      title={title}
+      subtitle={subtitle}
+      backgroundImage={featuredImage}
+    />
+    <section className="section">
+      <div className="container">
+        <Content source={section1} />
+      </div>
+    </section>
+
+    <section className="section">
+      <div className="container">
+        <Content source={section2} />
+      </div>
+    </section>
+  </main>
+)
+
+const DefaultPage = ({ data: { page } }) => (
+  <Layout
+    meta={page.frontmatter.meta || false}
+    title={page.frontmatter.title || false}
+  >
+    <DefaultPageTemplate {...page.frontmatter} body={page.html} />
+  </Layout>
+)
+export default DefaultPage
+
+export const pageQuery = graphql`
+  query DefaultPage($id: String!) {
+    page: markdownRemark(id: { eq: $id }) {
+      ...Meta
+      html
+      frontmatter {
+        title
+        template
+        subtitle
+        featuredImage
+        section1
+        section2
+      }
+    }
+  }
+`
