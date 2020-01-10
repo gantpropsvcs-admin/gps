627764dc29b5d9238f638f759ab2426178cf6d2a
gantpropsvcs-admin <58055804+gantpropsvcs-admin@users.noreply.github.com>
2020-01-10 13:27:01 -0700

    add services page, fix default template about page

---
+++ src/templates/ServicesPage.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+import PageHeader from '../components/PageHeader'
+import Content from '../components/Content'
+import Layout from '../components/Layout'
+import Accordion from '../components/Accordion'
+
+// Export Template for use in CMS preview
+export const ServicesPageTemplate = ({
+  title,
+  subtitle,
+  featuredImage,
+  section1,
+  section2,
+  body,
+}) => (
+  <main>
+    <PageHeader
+      title={title}
+      subtitle={subtitle}
+      backgroundImage={featuredImage}
+    />
+    <section className="section">
+      <div className="container">
+        <Content source={section1} />
+      </div>
+    </section>
+
+    <section className="section">
+      <div className="container">
+        <Accordion items={accordion} />
+      </div>
+    </section>
+
+    <section className="section">
+      <div className="container">
+        <Content source={section2} />
+      </div>
+    </section>
+  </main>
+)
+
+const ServicesPage = ({ data: { page } }) => (
+  <Layout
+    meta={page.frontmatter.meta || false}
+    title={page.frontmatter.title || false}
+  >
+    <ServicesPageTemplate {...page.frontmatter} body={page.html} />
+  </Layout>
+)
+export default ServicesPage
+
+export const pageQuery = graphql`
+  query ServicesPagePage($id: String!) {
+    page: markdownRemark(id: { eq: $id }) {
+      ...Meta
+      html
+      frontmatter {
+        title
+        template
+        subtitle
+        featuredImage
+        section1
+        section2
+        accordion {
+          title
+          description
+        }
+      }
+    }
+  }
+`
